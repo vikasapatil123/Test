7facd63d9c3dcac84184599d61370a4899dacef9
vikas patil <vikasapatil123@gmail.com>
2019-05-28 21:07:14 +0530

    c

--- src/com/sample/HelloWorld.java
+++ src/com/sample/HelloWorld.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
    
     for(int i=0;i<50;i++)
     {
-	 System.out.println("Hello World"+" i= "+i);
+	 System.out.println("Hello World.."+" i= "+i);
     }
    
 }
