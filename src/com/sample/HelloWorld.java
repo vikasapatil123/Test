1d51f73d39494ef0d146e89543e25b52638c2a11
vikas patil <vikasapatil123@gmail.com>
2019-05-28 18:27:53 +0530

    HelloWorld commited sucessfully

---
+++ src/com/sample/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.sample;
+
+public class HelloWorld {
+public static void main(String[] args) {
+   
+    for(int i=0;i<50;i++)
+    {
+	 System.out.println("Hello World"+" i="+i);
+    }
+   
+}
+}
